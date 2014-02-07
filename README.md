66cebf8b3e67a471dc044fd580eb019409eadb9f
Justin Pinili <jro.bot.email@gmail.com>
2014-02-06 23:12:27 -0800

    Uploading RoR. Installed RSpec and Factory Girls

---
+++ README.md
@@ -0,0 +1,5 @@
+# Show Archive (TBD)
+
+Capstone project
+
+Guided by my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShowArchive::Application.config.secret_key_base = 'f6184a4ae9d2b1df5f434221f5eb1f15c641233bcab090e8fde631bcc78c0288d121f247e05db621902ae87ec6e973ac30462bfece235c50c0bf2de3c7286c36'
