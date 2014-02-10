e3f5230b6e7a9db62af0f86bceec8cdff3b5e642
Justin Pinili <pinili.justin@gmail.com>
2014-02-10 00:49:11 -0800

    Updated submission of attendance but not populating event correctly

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -1,14 +1,17 @@
 class AttendsController < ApplicationController
   def new
     @attend = Attend.new
+    event = @attend.events.build
+    event.artists.build
+    event.venues.build
   end
 
   def create
     @attend = current_user.attends.build(attend_params)
 
     if @attend.save
-      session[:user_id] = @user.id
-      redirect_to user_url, notice: "Attendance added!"
+      session[:user_id] = User.find(session[:user_id])
+      redirect_to user_url(current_user), notice: "Attendance added!"
     else
       render "new"
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
   has_many :venues
-  has_many :artist
+  has_many :artists
 end
